701616ada1085e54cbe57f2b8760676756382597
JME <jme@JMEs-MacBook-Pro.local>
2025-05-20 23:58:04 +0800

    Fix session timeout redirect to point to root login page

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ export function requireLogin(req, res, next) {
     if (req.session && req.session.user) {
         return next();
     } else {
-        res.redirect('/login');
+        res.redirect('/');
     }
 }
 
